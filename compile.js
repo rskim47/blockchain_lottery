ab9effa584583665a087a595ee25493b05d58203
rskim47 <rickkim95@gmail.com>
2022-04-21 18:30:39 -0500

    more tests git push

--- compile.js
+++ compile.js
@@ -5,4 +5,5 @@ const solc = require("solc");
 const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
 const source = fs.readFileSync(lotteryPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+// Exporting Inbox Contract
+module.exports = solc.compile(source, 1).contracts[ ":Lottery" ];
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -18,4 +18,71 @@ describe('Lottery Contract', () => {
   it('deploys a contract', () => {
     assert.ok(lottery.options.address);
   });
+
+  it('Allows one account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[ 0 ],
+      value: web3.utils.toWei('0.02', 'ether') // eth => wei conversion
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[ 0 ]
+    });
+
+    assert.equal(accounts[ 0 ], players[ 0 ]);
+    assert.equal(1, players.length);
+  });
+
+  it('Allows multiple account to enter', async () => {
+    // Ading accounts 
+    for (let i = 0; i < accounts.length; i++) {
+      await lottery.methods.enter().send({
+        from: accounts[ i ],
+        value: web3.utils.toWei('0.02', 'ether') // eth => wei conversion
+      });
+    }
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[ 0 ]
+    });
+
+
+    for (let i = 0; i < accounts.length; i++) {
+      assert.equal(accounts[ i ], players[ i ]);
+    }
+    assert.equal(accounts.length, players.length);
+  });
+
+  it('requires minimum amount of ether', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[ 0 ],
+        value: 200
+      });
+      assert(false)
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('only manager can call pick winner', async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[ 1 ] // Non-manager account
+      });
+      assert(false)
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('sends money to the winner and resets the players array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[ 0 ],
+      value: web3.utils.toWei('2', 'ether') // eth => wei conversion
+    });
+
+    const initialBalance = await web3.eth.getBalance();
+
+  });
 });
